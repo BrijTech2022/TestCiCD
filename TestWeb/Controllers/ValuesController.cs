3e4940aa2788b78afa03f02d8d322c272bcb0d97
Sumit <sumit.it.sri@gmail.com>
2023-03-21 13:48:21 +0530

    New Update

--- TestWeb/Controllers/ValuesController.cs
+++ TestWeb/Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace TestWeb.Controllers
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "Kiezen softech";
+            return "Kiezen softech pvt.ltd";
         }
 
         // POST api/values
